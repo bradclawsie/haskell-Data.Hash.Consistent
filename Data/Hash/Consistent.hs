5bbf86258ea3b630f7acd7f61b99a44c6ea65920
brad clawsie <brad@b7j0c.org>
2012-10-15 22:56:55 -0700

    hlinted

--- Data/Hash/Consistent.hs
+++ Data/Hash/Consistent.hs
@@ -28,12 +28,12 @@ Here is a small program illustrating its use:
     ch <- return $ CH.add hosts n ch
     print $ show $ ch  
     let fh = [head hosts] :: [CH.Host]
-    let hh_fh = CH.hash_hosts fh n
+    let hh_fh = CH.hashHosts fh n
     print hh_fh
     ch <- return $ CH.del fh n ch
     print $ show $ ch  
     let i = 770931073
-    let tgt = CH.target_host i ch 
+    let tgt = CH.targetHost i ch 
     print tgt
     return ()
 @
@@ -45,7 +45,7 @@ https://www.b7j0c.org/stuff/license.txt
 
 -}
 
-module Data.Hash.Consistent (text_crc32,search,add,del,target_host,hash_hosts,
+module Data.Hash.Consistent (textCrc32,search,add,del,targetHost,hashHosts,
                              Host,Hash,HashHost,ConsistentHash) where
 
 import qualified Control.Monad.ST              as ST
@@ -63,46 +63,44 @@ type HashHost        = (Hash,Host)
 type ConsistentHash  = V.Vector HashHost 
 
 -- a convenience method to generate a crc for a string
-text_crc32 :: String -> Hash
-text_crc32 = CRC.crc32 . BU.fromString
+textCrc32 :: String -> Hash
+textCrc32 = CRC.crc32 . BU.fromString
 
 -- sort the consistent hash by the hash values
 sort :: ConsistentHash -> ConsistentHash
 sort v = ST.runST $ do
   m <- V.unsafeThaw v
   I.sortBy (O.comparing fst) m
-  v' <- V.unsafeFreeze m
-  return v'
+  V.unsafeFreeze m
 
 -- find the index of the first host hash greater than i
 search :: Hash -> ConsistentHash -> Int
 search i v = 
   ST.runST $ do 
     m <- V.unsafeThaw v
-    idx <- S.binarySearchBy (O.comparing fst) m (i,"")
-    return idx
+    S.binarySearchBy (O.comparing fst) m (i,"")
 
 -- locate which host should cache the item with key hash i
-target_host :: Hash -> ConsistentHash -> HashHost
-target_host i ch = let idx = search i ch 
-                       l   = V.length ch in
-                   if idx >= l then ch V.! 0 else ch V.! idx
+targetHost :: Hash -> ConsistentHash -> HashHost
+targetHost i ch = let idx = search i ch 
+                      l   = V.length ch in
+                  if idx >= l then ch V.! 0 else ch V.! idx
 
 new = V.empty :: ConsistentHash
 
 -- for a set of servers and a multiplying factor, return a list of
 -- crc hash values
-hash_hosts :: [Host] -> Int -> ConsistentHash
-hash_hosts hosts mult = 
-  V.fromList [(text_crc32 $ x++y,x)|x<-hosts,y<-map show [1..mult]] 
+hashHosts :: [Host] -> Int -> ConsistentHash
+hashHosts hosts mult = 
+  V.fromList [(textCrc32 $ x++y,x)|x<-hosts,y<-map show [1..mult]] 
 
 -- add a list of hosts to the consistent hash
 add :: [Host] -> Int -> ConsistentHash -> ConsistentHash
-add hosts mult ch = sort $ ch V.++ (hash_hosts hosts mult) 
+add hosts mult ch = sort $ ch V.++ hashHosts hosts mult 
 
 -- delete a list of hosts from the consistent hash
 del :: [Host] -> Int -> ConsistentHash -> ConsistentHash
-del hosts mult ch = let hh = hash_hosts hosts mult in
-  sort $ V.filter (\a -> V.notElem a hh) (ch) 
+del hosts mult ch = let hh = hashHosts hosts mult in
+  sort $ V.filter (`V.notElem` hh) ch 
 
   
